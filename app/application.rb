f1928483cd91515e5b4bc194855f7a64430bddf2
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-22 18:32:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,14 +8,14 @@ class Application
       item_name = req.path.split("/items/")
       item = @@items.find{|i| i.name == item_name}
 
-      if item.nil? 
+      if item.nil?
         resp.write "Item not found"
         resp.status = 400
       else
         resp.write item.price
       end
-        
-        
+
+
 
   end
 
